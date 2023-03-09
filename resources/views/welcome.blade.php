6381506a0c17d2edf6a637101fd749f462b2c454
JanGaud <JanisGaudreault.jg@gmail.com>
2023-03-08 19:26:15 -0500

    Table étudiant initialisé.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,22 +18,21 @@
 
 <body>
     <!-- Responsive navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark primary-color">
+    <nav class="navbar navbar-expand-lg primary-color">
         <div class="container px-5">
-            <a class="navbar-brand" href="#!"><img src="/img/UniHuntigntonWhite.png" alt=""></a>
+            <a class="navbar-brand" href="#!"><img src="/img/logoCollege.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Acceuil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Coin Social</a></li>
                 </ul>
+
             </div>
         </div>
     </nav>
     <!-- Page Content-->
-    <div class="container px-4 px-lg-5">
+    <div class="container px-4 px-lg-0">
         <!-- Heading Row-->
         <div class="row gx-4 gx-lg-5 align-items-center my-5">
             <div class="col-lg-7">
@@ -49,13 +48,16 @@
                     <!-- The slideshow -->
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="/img/pexels-matthis-volquardsen-2305098.jpg" alt="Los Angeles">
+                            <img src="/img/college1.jpg" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="/img/pexels-pixabay-159740.jpg" alt="Chicago">
+                            <img src="/img/college2.jpg" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="/img/pexels-pixabay-267885.jpg" alt="New York">
+                            <img src="/img/college3.jpg" alt="">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/img/college4.jpg" alt="">
                         </div>
                     </div>
 
@@ -69,21 +71,22 @@
 
                 </div>
             </div>
-            <div class="col-lg-5">
-                <h1 class="font-weight-light">Université Huntington</h1>
+            <div class="col-lg-5 mb-4">
+                <h1 class="font-weight-light">Collège de Maisonneuve</h1>
                 <p>
-                    L'Université Huntington est une institution d'enseignement supérieur de renommée mondiale      située sur l'île de Montréal. Elle se distingue par sa réputation pour l'excellence académique, la recherche de pointe et l'innovation technologique.
+                    Bienvenue sur la plateforme sociale du collège de Maisonneuve ! Nous sommes ravis de vous présenter un espace en ligne où les étudiants, les parents et les enseignants peuvent interagir, échanger des informations et rester informés de l'actualité de notre école.
                     <br><br>
-                    Cette école offre une gamme de programmes académiques, de la licence au doctorat, dans différents domaines tels que les sciences, les arts, les sciences sociales, les technologies, les affaires, la médecine et bien d'autres. Les étudiants ont accès à des laboratoires ultramodernes, des bibliothèques bien garnies et des installations sportives de première classe.
+                    En utilisant notre plateforme sociale, les étudiants peuvent trouver des informations sur les cours, les devoirs, les projets, les événements et les activités parascolaires, et interagir avec leurs amis et leurs enseignants en ligne.
                 </p>
-                <a class="btn btn-primary" href="#!">Call to Action!</a>
+                <a class="btn btn-primary" href="#!">En savoir plus!</a>
             </div>
         </div>
-        <!-- Call to Action-->
-        <div class="card text-white bg-secondary my-5 py-4 text-center">
-            <div class="card-body">
-                <p class="text-white m-0">Explorez votre potentiel illimité avec nous - L'université de choix pour votre avenir brillant !</p>
-            </div>
+    </div>
+        <!-- Parralax -->
+        <div class="parralax-container mt-5">
+            <section class="section-background parallax">
+            
+            </section>
         </div>
         <!-- Content Row-->
         <div class="container bootstrap snippets bootdeys">
@@ -92,8 +95,8 @@
                     <div class="card-big-shadow">
                         <div class="card card-just-text" data-background="color" data-color="blue" data-radius="none">
                             <div class="content">
-                                <h6 class="category">Best cards</h6>
-                                <h4 class="title"><a href="#">Blue Card</a></h4>
+                                <h6 class="category">04-22-2023</h6>
+                                <h4 class="title"><a href="#">Portes ouvertes</a></h4>
                                 <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p>
                             </div>
                         </div> <!-- end card -->
@@ -104,8 +107,8 @@
                     <div class="card-big-shadow">
                         <div class="card card-just-text" data-background="color" data-color="green" data-radius="none">
                             <div class="content">
-                                <h6 class="category">Best cards</h6>
-                                <h4 class="title"><a href="#">Green Card</a></h4>
+                                <h6 class="category">05-14-2023</h6>
+                                <h4 class="title"><a href="#">Collecte de sang</a></h4>
                                 <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p>
                             </div>
                         </div> <!-- end card -->
@@ -116,8 +119,8 @@
                     <div class="card-big-shadow">
                         <div class="card card-just-text" data-background="color" data-color="yellow" data-radius="none">
                             <div class="content">
-                                <h6 class="category">Best cards</h6>
-                                <h4 class="title"><a href="#">Yellow Card</a></h4>
+                                <h6 class="category">06-10-2023</h6>
+                                <h4 class="title"><a href="#">Conférence sur les relations</a></h4>
                                 <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p>
                             </div>
                         </div> <!-- end card -->
@@ -128,8 +131,8 @@
                     <div class="card-big-shadow">
                         <div class="card card-just-text" data-background="color" data-color="brown" data-radius="none">
                             <div class="content">
-                                <h6 class="category">Best cards</h6>
-                                <h4 class="title"><a href="#">Brown Card</a></h4>
+                                <h6 class="category">03-29-2023</h6>
+                                <h4 class="title"><a href="#">Final collégial de Hockey</a></h4>
                                 <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p>
                             </div>
                         </div> <!-- end card -->
@@ -140,8 +143,8 @@
                     <div class="card-big-shadow">
                         <div class="card card-just-text" data-background="color" data-color="purple" data-radius="none">
                             <div class="content">
-                                <h6 class="category">Best cards</h6>
-                                <h4 class="title"><a href="#">Purple Card</a></h4>
+                                <h6 class="category">02-16-2023</h6>
+                                <h4 class="title"><a href="#">Salon d'emploi</a></h4>
                                 <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p>
                             </div>
                         </div> <!-- end card -->
@@ -152,8 +155,8 @@
                     <div class="card-big-shadow">
                         <div class="card card-just-text" data-background="color" data-color="orange" data-radius="none">
                             <div class="content">
-                                <h6 class="category">Best cards</h6>
-                                <h4 class="title"><a href="#">Orange Card</a></h4>
+                                <h6 class="category">09-01-2023</h6>
+                                <h4 class="title"><a href="#">Journée de la culture</a></h4>
                                 <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p>
                             </div>
                         </div> <!-- end card -->
@@ -161,41 +164,52 @@
                 </div>
             </div>
             </div>
-        {{-- <div class="row gx-4 gx-lg-5">
-            <div class="col-md-4 mb-5">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h2 class="card-title">Card One</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
-                    </div>
-                    <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-5">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h2 class="card-title">Card Two</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
-                    </div>
-                    <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-5">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h2 class="card-title">Card Three</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
-                    </div>
-                    <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                </div>
-            </div>
-        </div> --}}
-    </div>
     <!-- Footer-->
-    <footer class="py-5 primary-color">
-        <div class="container px-4 px-lg-5">
-            <p class="m-0 text-center text-white">Copyright &copy; Université Huntington 2023</p>
+    <footer class="third-color">
+      <div class="container p-4">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 mb-4">
+            <h5 class="mb-3 text-white">Citation du jour</h5>
+            <p>
+                "La communication est le ciment qui maintient les relations unies et florissantes. Elle est comme une pluie fine qui arrose les âmes et les esprits, nourrissant les racines de l'empathie et de la compréhension. Sans une communication claire et ouverte, les ponts entre les personnes peuvent s'effondrer, laissant place à la méfiance, aux malentendus et à la solitude."
+            </p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4">
+            <h5 class="mb-3 text-white">liens</h5>
+            <ul class="list-unstyled mb-0">
+              <li class="mb-1">
+                <a href="#!" style="color: whitesmoke;">FAQ</a>
+              </li>
+              <li class="mb-1">
+                <a href="#!" style="color: whitesmoke;">Les enseignants</a>
+              </li>
+              <li class="mb-1">
+                <a href="#!" style="color: whitesmoke;">Les étudiants</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4">
+            <h5 class="mb-1 text-white">heures d'ouvertures</h5>
+            <table class="table" style="border-color: #ffffff;">
+              <tbody>
+                <tr>
+                  <td>Lun - Ven:</td>
+                  <td>7am - 10:30pm</td>
+                </tr>
+                <tr>
+                  <td>Sam - Dim:</td>
+                  <td>7am - 6pm</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
+      <div class="text-center p-3 secondary-color">
+        © 2023 Copyright:
+        <a class="text-white" href="https://www.cmaisonneuve.qc.ca/">Collège de Maisonneuve</a>
+      </div>
+      <!-- Copyright -->
     </footer>
     <!-- Bootstrap core JS-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
